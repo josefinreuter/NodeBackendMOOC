6eed2be0bbec8a19a1c7e87bdbace545d3a3a8ea
Josefin Reuter <josefin@josefinreuter.fi>
2018-07-02 14:22:45 +0300

    Osio 3, tehtävät 3.1-3.6

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Martti Tienari',
+    number: '040-123456',
+    id: 2
+  },
+  {
+    name: 'Lea Kutvonen',
+    number: '040-123456',
+    id: 3
+  }
+]
+
+const generateId = () => {
+  const id = Math.floor((Math.random() * 1000) + 1);
+  return id
+}
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if ( person ) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+if(body.name === undefined || body.number === undefined){
+
+  return response.status(400).json({error: 'name or number is missing'})
+
+}else if (persons.find(person => person.name === body.name) !== undefined){
+
+  return response.status(400).json({error: 'name must be unique'})
+
+}
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.get('/info', (req, res) => {  
+  const total = persons.length
+  const date = new Date()
+
+  res.send(`<p> Puhelinluettelossa on ${total} henkilön tiedot </p> <p> ${date} </p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
