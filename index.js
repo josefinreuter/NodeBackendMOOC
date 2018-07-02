383d0486d24523fb8b7a934bcfbcde6a2a5b6702
Josefin Reuter <josefin@josefinreuter.fi>
2018-07-02 18:23:41 +0300

    updates

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/info', (req, res) => {
     res.send(`<p> Puhelinluettelossa on ${total} henkilön tiedot </p> <p> ${date} </p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
