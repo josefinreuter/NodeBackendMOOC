e684abe43253cebf660824835e59d7b0e705d0a9
Josefin Reuter <josefin@josefinreuter.fi>
2018-07-04 11:53:05 +0300

    Osio 3 loppuun

--- models/person.js
+++ models/person.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
 const personSchema = new Schema({
-	name: String,
-        number: String
+    name: String,
+    number: String
 })
 
 personSchema.statics.format = function (person) {
-   const formattedPerson = { ...person._doc, id: person._id }
-   delete formattedPerson._id
-   delete formattedPerson.__v
+    const formattedPerson = {...person._doc, id: person._id}
+    delete formattedPerson._id
+    delete formattedPerson.__v
 
-   return formattedPerson
+    return formattedPerson
 }
 
 const Person = mongoose.model('Person', personSchema)
